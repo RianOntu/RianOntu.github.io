cfc4a699222b86f6697cb5416cb6533a9f496691
Rian Ontu <60427804+RianOntu@users.noreply.github.com>
2020-12-20 20:21:42 +0600

    Number guessing game

---
+++ site3/script.js
@@ -0,0 +1,64 @@
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+ 
+
+  for(let i=0;i<=2;i++){
+
+  
+
+if(i==0){
+    var prom=prompt("Guess a Number between 0 to 10! You have three chances");
+    let correct_ans=getRandomInt(11);
+    let bigger=prom<correct_ans;
+   let smaller=prom>correct_ans;
+        if(bigger==true){
+            alert('The answer is bigger than your guessed one!');
+        }
+        else if(smaller==true){
+            alert('The answer is smaller than your guesses one!');
+        }
+        else if(correct_ans==prom)
+        {
+            alert("You win!")
+        }
+}
+if(i==1){
+    var prom=prompt("You have two chances left!");
+    let correct_ans=getRandomInt(11);
+    let bigger=prom<correct_ans;
+   let smaller=prom>correct_ans;
+    if(bigger==true){
+        alert('The answer is bigger than your guessed one!');
+    }
+    else if(smaller==true){
+        alert('The answer is smaller than your guesses one!');
+    }
+    else if(correct_ans==prom)
+    {
+        alert("You win!")
+    }
+}
+if(i==2){
+    var prom=prompt("Last Chance!");
+    let correct_ans=getRandomInt(11);
+    let bigger=prom<correct_ans;
+   let smaller=prom>correct_ans;
+    if(bigger==true){
+        alert('You lose');
+    }
+    else if(smaller==true){
+        alert('You lose!');
+    }
+    else if(correct_ans==prom)
+    {
+        alert("You win!")
+    }
+
+}
+  }
+
